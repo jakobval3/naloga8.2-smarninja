41a27be19f35f3e85f2d0076d2f472095a8a385d
tilenavs <tilenavs@gmail.com>
2022-09-29 19:17:30 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+secret = 5
+guess = int(input("Please enter your number here:"))
+if guess == secret:
+    print("Congratulations,you guessed the right number!")
+else:
+    print("Sorry, you guessed the wrong number.")
